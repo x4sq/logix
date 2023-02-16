7a2ec27ea9596ff410498e7c481ea08f0c10f9d3
ryan <50450776+x4sq@users.noreply.github.com>
2023-02-16 00:02:17 -0600

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv/config')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.on('ready', () =>{
+    console.log('on')
+})
+
+client.login(process.env.TOKEN)
