cd4c5e95e50390be7f48a55dabad675bbe601da2
ryan <50450776+x4sq@users.noreply.github.com>
2023-02-16 00:25:41 -0600

    handlers and events and other stuff

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-require('dotenv/config')
-
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-})
+	intents: [
+		GatewayIntentBits.Guilds, 
+		GatewayIntentBits.GuildMessages, 
+		GatewayIntentBits.GuildPresences, 
+		GatewayIntentBits.GuildMessageReactions, 
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent
+	], 
+	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
+});
 
-client.on('ready', () =>{
-    console.log('on')
-})
+const fs = require('fs');
+const config = require('./config.json');
+require('dotenv').config()
+
+client.commands = new Collection()
+client.aliases = new Collection()
+client.slashCommands = new Collection();
+client.buttons = new Collection();
+client.prefix = config.prefix;
+
+module.exports = client;
+
+
+fs.readdirSync('./handlers').forEach((handler) => {
+  require(`./handlers/${handler}`)(client)
+});
+
 
 client.login(process.env.TOKEN)
