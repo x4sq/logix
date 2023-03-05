2c3cf7393b92360915d7734a75b317e2438aa7df
ryan <50450776+x4sq@users.noreply.github.com>
2023-03-05 02:18:14 -0600

    unfinished blackjack, added uptime and added min withdraw

--- schemas/balance.js
+++ schemas/balance.js
@@ -1,9 +1,9 @@
 const { model, Schema, mongoose } = require('mongoose')
 
-let balSchema = new Schema({
+let balSchemaTEST = new Schema({
     UserID: String,
     Balance: String,
     ClaimedDaily: Boolean
 })
 
-module.exports = model('balSchema', balSchema)
+module.exports = model('balSchemaTest', balSchemaTEST)
