8fcfff953f4daa9877781eec17bfce72bf90e986
ryan <50450776+x4sq@users.noreply.github.com>
2023-03-05 02:54:00 -0600

    improve uptime

--- slashCommands/info/uptime.js
+++ slashCommands/info/uptime.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandType } = require('discord.js');
+const { ApplicationCommandType, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: 'uptime',
@@ -7,10 +7,12 @@ module.exports = {
 	cooldown: 3000,
 	run: async (client, interaction) => {
         const seconds = Math.round(Date.now() / 1000);
-        await interaction.reply(
-          `Bot has been up since <t:${Math.round(
-            seconds - process.uptime()
-          )}:R>`
-        );
-	}
-};
+        await interaction.reply({embeds: [
+          new EmbedBuilder()
+          .setColor('DarkButNotBlack')
+          .setFooter({ text:  `Create a ticket if this was an error. ID: ${interaction.user.id}` })
+          .setDescription(`Bot has been up since <t:${Math.round(seconds - process.uptime())}:R>`)
+        ]
+      })
+    }
+}
